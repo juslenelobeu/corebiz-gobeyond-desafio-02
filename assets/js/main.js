7427787755f7e272f0c64f89d8b77b8a9960b7dd
juslenelobeu <juslenelobeu@gmail.com>
2021-09-16 18:50:36 -0300

    create table and implementation script for get values of form and print on table

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-// header full
+// open form
 const openForm = () => {
   const header = document.querySelector(".header");
   const btnHeader = document.querySelector("#btn-header");
@@ -11,7 +11,7 @@ const openForm = () => {
   btnHeader.style.display = "none";
   main.style.display = "flex";
 };
-
+// close form
 const closeForm = () => {
   const header = document.querySelector(".header");
   const btnHeader = document.querySelector("#btn-header");
@@ -23,3 +23,48 @@ const closeForm = () => {
   btnHeader.style.display = "inline-block";
   main.style.display = "none";
 };
+
+let funcionarios = [];
+
+const saveForm = (e) => {
+  e.preventDefault();
+
+  // get inputs
+  const inputNome = document.querySelector("#input-nome").value;
+  const inputSalario = parseFloat(
+    document.querySelector("#input-salario").value
+  );
+  const inputVendas = parseFloat(document.querySelector("#input-vendas").value);
+  const inputMes = document.querySelector("#select-mes").value;
+
+  const table = document.querySelector(".table");
+
+  const funcionario = {
+    nome: inputNome,
+    salario: inputSalario,
+    vendas: inputVendas,
+    comissao: (10 / 100) * inputVendas,
+    mes: inputMes,
+  };
+
+  table.classList.add("visible");
+  funcionarios.push(funcionario);
+
+  const tableBody = document.querySelector(".table__body");
+  tableBody.innerHTML = "";
+  funcionarios.forEach((item) => {
+    tableBody.innerHTML += `
+      <tr>
+        <td>${item.nome}</td>
+        <td>${item.mes}</td>
+        <td>${item.salario.toFixed(2)}</td>
+        <td>${item.vendas.toFixed(2)}</td>
+        <td>${(item.comissao + item.salario).toFixed(2)}</td>
+      </tr>
+    `;
+  });
+  console.log(funcionarios);
+};
+
+const form = document.querySelector("#form");
+form.addEventListener("submit", saveForm);
